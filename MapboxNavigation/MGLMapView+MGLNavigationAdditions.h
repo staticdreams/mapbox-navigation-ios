3b76603c83acd7ee320562a802f2be293e73f48b
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2017-07-20 17:53:20 +0200

    wip follow with course

--- MapboxNavigation/MGLMapView+MGLNavigationAdditions.h
+++ MapboxNavigation/MGLMapView+MGLNavigationAdditions.h
@@ -8,6 +8,13 @@
 // FIXME: This will be removed once https://github.com/mapbox/mapbox-navigation-ios/issues/352 is implemented.
 - (void)validateLocationServices;
 
+- (void)_setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate edgePadding:(UIEdgeInsets)insets zoomLevel:(double)zoomLevel direction:(CLLocationDirection)direction duration:(NSTimeInterval)duration animationTimingFunction:(nullable CAMediaTimingFunction *)function completionHandler:(nullable void (^)(void))completion;
+
+- (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
+                edgePadding:(UIEdgeInsets)edgePadding
+                  zoomLevel:(double)zoomLevel
+                  direction:(CLLocationDirection)direction;
+
 @property (nonatomic, readonly) CLLocationManager *locationManager;
 
 @end
--- MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
+++ MapboxNavigation/MGLMapView+MGLNavigationAdditions.m
@@ -1,5 +1,6 @@
 #import "MGLMapView+MGLNavigationAdditions.h"
 
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wincomplete-implementation"
 // Supressing compiler warning until https://github.com/mapbox/mapbox-gl-native/issues/6867 is implemented
@@ -8,4 +9,19 @@
 
 @dynamic locationManager;
 
+
+- (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate
+                edgePadding:(UIEdgeInsets)edgePadding
+                  zoomLevel:(double)zoomLevel
+                  direction:(CLLocationDirection)direction
+{
+    [self _setCenterCoordinate:centerCoordinate
+                   edgePadding:edgePadding
+                     zoomLevel:zoomLevel
+                     direction:direction
+                      duration:0
+       animationTimingFunction:nil
+             completionHandler:nil];
+}
+    
 @end
